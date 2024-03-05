6bf7ec750f2b72bdd412655e71f371ac62da4e71
Gus <gustavo.silvapes2020@gmail.com>
2024-03-05 07:47:44 -0300

    Feature: Modularizando aplicação

--- src/domain/event/@shared/event-dispatcher.interface.ts
+++
@@ -1,9 +0,0 @@
-import EventHandlerInterface from "./event-handler.interface";
-import EventInterface from "./event.interface";
-
-export default interface EventDispatcherInterface {
-    notify(event: EventInterface): void;
-    register(enventName: string, eventHandler: EventHandlerInterface): void;
-    unregister(eventName: string, eventHandler: EventHandlerInterface): void;
-    unregisterAll(): void;
-}
